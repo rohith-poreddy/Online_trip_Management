5e0442c1a6cfd56aabdf8d4a72545df58962c8c4
rohith-poreddy <rohithreddyporeddy@gmail.com>
2021-08-14 11:47:04 +0530

    2c

---
+++ tripmanagementbackend/src/main/java/com/example/demo/TripmanagementbackendApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class TripmanagementbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmanagementbackendApplication.class, args);
+	}
+
+	
+
+}
